9abd01623994205f47cde665a5390e314bd1e574
jafludo <jaf.ludo@hotmail.fr>
2023-03-15 00:18:05 +0100

    addexportdata

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'; // Converti les modules CommonJS en ES6
 import vue from 'rollup-plugin-vue'; // Gère les composants .vue
 import buble from '@rollup/plugin-buble'; // Permet un polyfill de notre code pour un meilleur support sur les navigateurs
-
 export default {
   input: 'src/wrapper.js', // Fichier relatif en partant de notre fichier package.json
   output: {
