a45c3065466287749956efa38a314e4cfe0ed94a
jafludo <jaf.ludo@hotmail.fr>
2023-03-28 11:35:28 +0200

    impl

--- src/ges.vue
+++ src/ges.vue
@@ -2,27 +2,26 @@
     <div id="ges_div" class="flex">
         <div class="div_left">
             <div class="legend_top">
-                <p>Logement économe</p>
+                <p>{{ title_haut }}</p>
             </div>
             <div id='etiquette_ener' class="etiquette">
-                <div v-for="(e, index) in dpe" :key="refreshGES">
+                <div v-for="(e, index) in ges">
                     <div class="grp-et">
                         <div class="etiquette_base" :style="'background : ' + e.color + ' ; width: ' + e.width">
                             <p :style="'color : ' + e.colortext + ' ; padding-left: 8px'">{{ e.text }}</p>
                             <p :style="'color : ' + e.colortext + ' ; padding-right: 8px'">{{ e.letter }}</p>
                         </div>
-                        <div class="triangle-code" :style="'border-left: 24px solid ' + e.color + ';'"></div>
                     </div>
                     <p class="space_between_class"></p>
                 </div>
             </div>
             <div class="legend_bottom">
-                <p>Logement énergivore</p>
+                <p>{{ title_bas }}</p>
             </div>
         </div>
         <div class="div_right">
             <div class="legend_top_r">
-                Logement
+                {{ typeBatiment }}
             </div>
             <div class="space_beetween"></div>
             <div class="grp-et">
@@ -54,60 +53,63 @@ export default {
     data() {
         return {
             ges: [
-                { text: "≤ 5", letter: "A", color: "#f2eff4" },
-                { text: "6 à 15", letter: "B", color: "#dfc1f7" },
-                { text: "16 à 30", letter: "C", color: "#d6aaf4" },
-                { text: "31 à 60", letter: "D", color: "#cc93f4" },
-                { text: "61 à 100", letter: "E", color: "#bb72f3" },
-                { text: "101 à 145", letter: "F", color: "#a94cee" },
-                { text: "> 145", letter: "G", color: "#8b1ae1" },
+                { text: "≤ 5", letter: "A", color: "#f2eff4", width: '30%', colortext: 'black', minrange: 0, maxrange: 5, min: 0 },
+                { text: "6 à 10", letter: "B", color: "#dfc1f7", width: '40%', colortext: 'black', minrange: 6, maxrange: 10, min: 36 },
+                { text: "11 à 20", letter: "C", color: "#d6aaf4", width: '50%', colortext: 'black', minrange: 11, maxrange: 20, min: 72 },
+                { text: "21 à 35", letter: "D", color: "#cc93f4", width: '60%', colortext: 'black', minrange: 21, maxrange: 35, min: 108 },
+                { text: "36 à 55", letter: "E", color: "#bb72f3", width: '70%', colortext: 'black', minrange: 36, maxrange: 55, min: 144 },
+                { text: "56 à 80", letter: "F", color: "#a94cee", width: '80%', colortext: 'black', minrange: 56, maxrange: 80, min: 180 },
+                { text: "> 80", letter: "G", color: "#8b1ae1", width: '90%', colortext: 'white', minrange: 80, maxrange: 9999, min: 216 },
             ],
             gesTertiaire: [
-                { text: "≤ 5", letter: "A", color: "#f2eff4" },
-                { text: "6 à 15", letter: "B", color: "#dfc1f7" },
-                { text: "16 à 30", letter: "C", color: "#d6aaf4" },
-                { text: "31 à 60", letter: "D", color: "#cc93f4" },
-                { text: "61 à 100", letter: "E", color: "#bb72f3" },
-                { text: "101 à 145", letter: "F", color: "#a94cee" },
-                { text: "> 145", letter: "G", color: "#8b1ae1" },
+                { text: "≤ 5", letter: "A", color: "#f2eff4", width: '45%', colortext: 'black', minrange: 0, maxrange: 5, min: 0 },
+                { text: "6 à 10", letter: "B", color: "#dfc1f7", width: '50%', colortext: 'black', minrange: 6, maxrange: 10, min: 36 },
+                { text: "11 à 20", letter: "C", color: "#d6aaf4", width: '55%', colortext: 'black', minrange: 11, maxrange: 20, min: 72 },
+                { text: "21 à 35", letter: "D", color: "#cc93f4", width: '60%', colortext: 'black', minrange: 21, maxrange: 35, min: 108 },
+                { text: "36 à 55", letter: "E", color: "#bb72f3", width: '65%', colortext: 'black', minrange: 36, maxrange: 55, min: 144 },
+                { text: "56 à 80", letter: "F", color: "#a94cee", width: '70%', colortext: 'black', minrange: 56, maxrange: 80, min: 180 },
+                { text: "81 à 110", letter: "G", color: "#8b1ae1", width: '75%', colortext: 'white', minrange: 81, maxrange: 110, min: 216 },
+                { text: "111 à 145", letter: "H", color: "#4d4d4d", width: '80%', colortext: 'white', minrange: 111, maxrange: 145, min: 252 },
+                { text: "> 145", letter: "I", color: "#000000", width: '85%', colortext: 'white', minrange: 145, maxrange: 9999, min: 288 },
             ],
             gesPublic: [
-                { text: "≤ 5", letter: "A", color: "#f2eff4" },
-                { text: "6 à 15", letter: "B", color: "#dfc1f7" },
-                { text: "16 à 30", letter: "C", color: "#d6aaf4" },
-                { text: "31 à 60", letter: "D", color: "#cc93f4" },
-                { text: "61 à 100", letter: "E", color: "#bb72f3" },
-                { text: "101 à 145", letter: "F", color: "#a94cee" },
-                { text: "> 145", letter: "G", color: "#8b1ae1" },
+                { text: "≤ 3", letter: "A", color: "#f2eff4", width: '30%', colortext: 'black', minrange: 0, maxrange: 3, min: 0 },
+                { text: "4 à 10", letter: "B", color: "#dfc1f7", width: '40%', colortext: 'black', minrange: 4, maxrange: 10, min: 36 },
+                { text: "11 à 25", letter: "C", color: "#d6aaf4", width: '50%', colortext: 'black', minrange: 11, maxrange: 25, min: 72 },
+                { text: "26 à 45", letter: "D", color: "#cc93f4", width: '60%', colortext: 'black', minrange: 26, maxrange: 45, min: 108 },
+                { text: "46 à 70", letter: "E", color: "#bb72f3", width: '70%', colortext: 'black', minrange: 46, maxrange: 70, min: 144 },
+                { text: "71 à 95", letter: "F", color: "#a94cee", width: '80%', colortext: 'black', minrange: 71, maxrange: 95, min: 180 },
+                { text: "> 95", letter: "G", color: "#8b1ae1", width: '90%', colortext: 'white', minrange: 95, maxrange: 9999, min: 216 },
             ],
             gesBureaux: [
-                { text: "≤ 5", letter: "A", color: "#f2eff4" },
-                { text: "6 à 15", letter: "B", color: "#dfc1f7" },
-                { text: "16 à 30", letter: "C", color: "#d6aaf4" },
-                { text: "31 à 60", letter: "D", color: "#cc93f4" },
-                { text: "61 à 100", letter: "E", color: "#bb72f3" },
-                { text: "101 à 145", letter: "F", color: "#a94cee" },
-                { text: "> 145", letter: "G", color: "#8b1ae1" },
+                { text: "≤ 5", letter: "A", color: "#f2eff4", width: '30%', colortext: 'black', minrange: 0, maxrange: 5, min: 0 },
+                { text: "6 à 15", letter: "B", color: "#dfc1f7", width: '40%', colortext: 'black', minrange: 6, maxrange: 15, min: 36 },
+                { text: "16 à 30", letter: "C", color: "#d6aaf4", width: '50%', colortext: 'black', minrange: 16, maxrange: 30, min: 72 },
+                { text: "31 à 60", letter: "D", color: "#cc93f4", width: '60%', colortext: 'black', minrange: 31, maxrange: 60, min: 108 },
+                { text: "61 à 100", letter: "E", color: "#bb72f3", width: '70%', colortext: 'black', minrange: 61, maxrange: 100, min: 144 },
+                { text: "101 à 145", letter: "F", color: "#a94cee", width: '80%', colortext: 'black', minrange: 101, maxrange: 145, min: 180 },
+                { text: "> 145", letter: "G", color: "#8b1ae1", width: '90%', colortext: 'white', minrange: 145, maxrange: 9999, min: 216 },
             ],
             gesOccContinue: [
-                { text: "≤ 5", letter: "A", color: "#f2eff4" },
-                { text: "6 à 15", letter: "B", color: "#dfc1f7" },
-                { text: "16 à 30", letter: "C", color: "#d6aaf4" },
-                { text: "31 à 60", letter: "D", color: "#cc93f4" },
-                { text: "61 à 100", letter: "E", color: "#bb72f3" },
-                { text: "101 à 145", letter: "F", color: "#a94cee" },
-                { text: "> 145", letter: "G", color: "#8b1ae1" },
+                { text: "≤ 12", letter: "A", color: "#f2eff4", width: '30%', colortext: 'black', minrange: 0, maxrange: 12, min: 0 },
+                { text: "13 à 30", letter: "B", color: "#dfc1f7", width: '40%', colortext: 'black', minrange: 13, maxrange: 30, min: 36 },
+                { text: "31 à 65", letter: "C", color: "#d6aaf4", width: '50%', colortext: 'black', minrange: 31, maxrange: 65, min: 72 },
+                { text: "66 à 110", letter: "D", color: "#cc93f4", width: '60%', colortext: 'black', minrange: 66, maxrange: 110, min: 108 },
+                { text: "111 à 160", letter: "E", color: "#bb72f3", width: '70%', colortext: 'black', minrange: 111, maxrange: 160, min: 144 },
+                { text: "161 à 220", letter: "F", color: "#a94cee", width: '80%', colortext: 'black', minrange: 161, maxrange: 220, min: 180 },
+                { text: "> 220", letter: "G", color: "#8b1ae1", width: '90%', colortext: 'white', minrange: 220, maxrange: 9999, min: 216 },
             ],
             assocGES: [
-                { type: 'logement', ref: 'ges' },
-                { type: 'tertiaire', ref: 'gesTertiaire' },
-                { type: 'public', ref: 'gesPublic' },
-                { type: 'bureaux', ref: 'gesBureaux' },
-                { type: 'OccContinue', ref: 'gesOccContinue' },
+                { type: 'logement', ref: 'ges', title_haut: 'Faible émission de GES', title_bas: 'Forte émission de GES', typeBatiment: 'Logement' },
+                { type: 'tertiaire', ref: 'gesTertiaire', title_haut: 'Faible émission de GES', title_bas: 'Forte émission de GES', typeBatiment: 'Bâtiment' },
+                { type: 'public', ref: 'gesPublic', title_haut: 'Faible émission de GES', title_bas: 'Forte émission de GES', typeBatiment: 'Bâtiment' },
+                { type: 'bureaux', ref: 'gesBureaux', title_haut: 'Faible émission de GES', title_bas: 'Forte émission de GES', typeBatiment: 'Bâtiment' },
+                { type: 'OccContinue', ref: 'gesOccContinue', title_haut: 'Faible émission de GES', title_bas: 'Forte émission de GES', typeBatiment: 'Bâtiment' },
             ],
             widthDaron: 0,
-            exportMode: null,
-            refreshGES: 0
+            title_haut: '',
+            title_bas: '',
+            typeBatiment: ''
         }
     },
     created() {
@@ -116,16 +118,18 @@ export default {
     },
     mounted() {
         let gesfind = this.typeGES ? this.assocGES.find(ass => ass.type == this.typeGES) : this.ges;
-        this.typeDPE ? this.dpe = this[gesfind.ref] : this.ges;
+        this.typeGES ? this.ges = this[gesfind.ref] : this.ges;
         gesfind = this.typeGES ? this.assocGES.find(ass => ass.type == this.typeGES) : this.fes;
         this.typeGES ? this.ges = this[gesfind.ref] : this.ges;
+        gesfind ? this.title_haut = gesfind.title_haut : '';
+        gesfind ? this.title_bas = gesfind.title_bas : '';
+        gesfind ? this.typeBatiment = gesfind.typeBatiment : '';
         let gesrange = this.ges.find(item => item.minrange <= this.valueGES && item.maxrange >= this.valueGES);
         gesrange == undefined ? gesrange = this.ges[this.ges.length - 1] : '';
         this.widthDaron = document.getElementById('ges_div').offsetWidth;
         let DynamicHeight = gesrange.min;
         var r = document.querySelector(':root');
         r.style.setProperty('--height', (DynamicHeight) + "px");
-        this.refreshGES++;
     },
     computed: {
     },
@@ -203,12 +207,8 @@ export default {
             align-items: center;
             justify-content: space-between;
             position: relative;
+            border: 1px solid;
         }
-    }
-
-    .triangle-code {
-        border-top: 16px solid transparent;
-        border-bottom: 16px solid transparent;
     }
 
     .triangle-leftcode {
